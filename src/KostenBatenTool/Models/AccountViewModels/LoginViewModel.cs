e6305921c8fab71ecc48f169eaeb1ec920ada8dc
Lotte Jespers <lotje.j@hotmail.com>
2017-02-23 09:03:09 +0100

    css Inlog en Registreren

--- src/KostenBatenTool/Models/AccountViewModels/LoginViewModel.cs
+++ src/KostenBatenTool/Models/AccountViewModels/LoginViewModel.cs
@@ -16,7 +16,7 @@ namespace KostenBatenTool.Models.AccountViewModels
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Wachtwoord onthouden")]
         public bool RememberMe { get; set; }
     }
 }
